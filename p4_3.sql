16d42d02f14d7a56d0b34dd966348d0f4e6e3ac6
Serhii Osmolovskyi <109869085+SerhiiOsmolovskyi@users.noreply.github.com>
2024-04-02 12:28:55 -0600

    Add files via upload

---
+++ p4_3.sql
@@ -0,0 +1,10 @@
+SELECT *
+FROM orders
+INNER JOIN order_details ON orders.id = order_details.order_id
+INNER JOIN customers ON orders.customer_id = customers.id
+INNER JOIN products ON order_details.product_id = products.id
+INNER JOIN categories ON products.category_id = categories.id
+INNER JOIN employees ON orders.employee_id = employees.employee_id
+INNER JOIN shippers ON orders.shipper_id = shippers.id
+INNER JOIN suppliers ON products.supplier_id = suppliers.id
+WHERE employees.employee_id > 3 AND employees.employee_id <= 10;
---
+++ p4_5.sql
@@ -0,0 +1,16 @@
+SELECT 
+    categories.name AS category_name,
+    COUNT(*) AS row_count,
+    AVG(order_details.quantity) AS avg_quantity
+FROM 
+    orders
+INNER JOIN order_details ON orders.id = order_details.order_id
+INNER JOIN products ON order_details.product_id = products.id
+INNER JOIN categories ON products.category_id = categories.id
+INNER JOIN employees ON orders.employee_id = employees.employee_id
+INNER JOIN shippers ON orders.shipper_id = shippers.id
+INNER JOIN suppliers ON products.supplier_id = suppliers.id
+GROUP BY 
+    categories.name
+HAVING
+    avg_quantity > 21;
